7833dc60285e9cfddfcedd4ecfad02f90799a699
Andres Vargas <andrevargas@coca-cola.com>
2020-06-06 18:14:47 -0500

    Finish ! lest document

--- main.py
+++ main.py
@@ -3,33 +3,50 @@ import asyncio
 import lxml.html
 from magnet2torrent import Magnet2Torrent
 from scrap import main as scrap
-from tabulate import tabulate
-
+import tqdm
+import tabulate
 
 URL = "https://nyaa.si/?&q=puya&p={}"
 
+
+async def task(item, session):
+    td = item.xpath("./td")
+    title = td[1].text_content().strip()
+    magneturi = td[2].xpath("./a[2]/@href").pop()
+    m = Magnet2Torrent(magneturi)
+    infohash = m.infohash.hex()
+    result = await scrap(session, {"title": title, "infohash": infohash})
+    return result
+
+async def pretask(item):
+    s = lambda x,y: x.get("stats", {}).get(y,"")
+    return {
+        "title": item.get("title"),
+#        "hash": item.get("infohash"),
+        "complete": s(item, "complete"),
+        "seeds": s(item, "seeds"),
+        "peers": s(item, "peers"),
+    }.values()
+
+
+
 async def main():
-    table = []
+    results = []
+    tasks = []
     async with aiohttp.ClientSession() as session:
-        for page in range(0, 20):
+        for page in tqdm.tqdm(range(0, 20), desc="Pages"):
             result = await session.get(URL.format(page))
             page_content = await result.text()
             root = lxml.html.fromstring(page_content)
             items = root.xpath("//table/tbody/tr")
             for i in items:
-                td = i.xpath("./td")
-                title = td[1].text_content().strip()
-                magneturi = td[2].xpath("./a[2]/@href").pop()
-                m = Magnet2Torrent(magneturi)
-                infohash = m.infohash.hex()
-                result = await scrap(session, infohash)
-                table.append(list(result.items()) + ['name',title])
-    print(tabulate(table))
-
-
+                tasks.append(task(i, session))
+        for f in tqdm.tqdm(asyncio.as_completed(tasks), total=len(tasks),
+                               desc="Executing task"):
+            results.append(await pretask(await f))
+    print(tabulate.tabulate(results, headers=["title", "complete", "seeds", "peers"]))
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-        
--- scrap.py
+++ scrap.py
@@ -4,21 +4,27 @@ import bencode
 
 TRACKER_URL = "http://nyaa.tracker.wf:7777/announce"
 
-async def main(session, info_hash):
+
+async def main(session, object_dict):
     tracker_url = TRACKER_URL.replace("announce", "scrape")
+    info_hash = object_dict.get("infohash")
     info = urllib.parse.quote_plus(binascii.unhexlify(info_hash))
     url = f"{tracker_url}?info_hash={info}"
     resp = await session.get(url)
     content = await resp.read()
     d = bencode.bdecode(content)
     ret = {}
-    print(d)
-    for hash, stats in d[b'files'].items():
-        nice_hash = binascii.b2a_hex(hash).decode("utf-8")
-        s = stats[b"complete"]
-        p = stats[b"incomplete"]
-        c = stats[b"downloaded"]
-        ret[nice_hash] = {"seeds": s, "peers": p, "complete": c}
+    if b'files' in d:
+        for hash, stats in d[b'files'].items():
+            nice_hash = binascii.b2a_hex(hash).decode("utf-8")
+            s = stats[b"complete"]
+            p = stats[b"incomplete"]
+            c = stats[b"downloaded"]
+            ret["stats"] = {"seeds": s, "peers": p, 
+                              "complete": c, 
+                              }
+    ret.update(object_dict)
+#    print(ret)
     return ret
 
 
